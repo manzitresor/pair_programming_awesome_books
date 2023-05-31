51df8b556a2c92cf135e792246298a26e4bfe23f
kerlosbahaa3 <aboelking4@gmail.com>
2023-05-31 15:00:13 +0300

    finish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class AwesomeBooks {
+class awesomeBooks {
   books;
 
   booksContainer;
@@ -6,27 +6,26 @@ class AwesomeBooks {
   title;
 
   author;
-
+  /* eslint-disable */
   constructor() {
     this.books = [];
-    this.booksContainer = document.getElementById('books');
-    this.title = document.getElementById('title');
-    this.author = document.getElementById('author');
+    this.booksContainer = document.getElementById("books");
+    this.title = document.getElementById("title");
+    this.author = document.getElementById("author");
   }
 
   localBooks(books) {
-    books = this.books;
-    const storage = JSON.parse(localStorage.getItem('Books'));
+    const storage = JSON.parse(localStorage.getItem("Books"));
     return storage || books;
   }
 
   displayBooks(booksList) {
-    this.booksContainer.innerHTML = '';
+    this.booksContainer.innerHTML = "";
     for (let i = 0; i < booksList.length; i += 1) {
       this.booksContainer.innerHTML += `
             <br>
             <div class="main-div">  <p class="pargraph-books">"${booksList[i].title}" by ${booksList[i].author}</p>
-            <div class="buton-books"> <button  id="remove${i}" onclick="awesomeBook.removeItem(${i})" >Remove</button><br/></div> 
+            <div class="buton-books"> <button  id="remove${i}" onclick="awesome_books.removeItem(${i})" >Remove</button><br/></div> 
             <br/></div>
           `;
     }
@@ -40,27 +39,48 @@ class AwesomeBooks {
         title: this.title.value,
         author: this.author.value,
       });
-      localStorage.setItem('Books', JSON.stringify(this.books));
+      localStorage.setItem("Books", JSON.stringify(this.books));
       this.displayBooks(this.localBooks(this.books));
-      this.title.value = '';
-      this.author.value = '';
+      this.title.value = "";
+      this.author.value = "";
     }
   }
-
+  /* eslint-disable */
   removeItem(items) {
     if (items !== undefined) {
       const localItem = this.localBooks(this.books);
       const removedItem = localItem.filter(
-        (item) => item.id !== localItem[items].id,
+        (item) => item.id !== localItem[items].id
       );
-      localStorage.setItem('Books', JSON.stringify(removedItem));
-      return this.displayBooks(this.localBooks(this.books));
+      localStorage.setItem("Books", JSON.stringify(removedItem));
+      return this.displayBooks(this.localBooks(books));
     }
-    return this.displayBooks(this.localBooks(this.books));
+    return this.displayBooks(this.localBooks(books));
   }
 }
-const awesomeBook = new AwesomeBooks();
-awesomeBook.removeItem();
-document.getElementById('add').addEventListener('click', () => {
-  awesomeBook.adding();
+
+const awesome_books = new awesomeBooks();
+awesome_books.removeItem();
+document.getElementById("add").addEventListener("click", () => {
+  awesome_books.adding();
+});
+
+document.getElementById("bookInput").style.display = "none";
+document.getElementById("contactInfo").style.display = "none";
+
+document.getElementById("listLink").addEventListener("click", () => {
+  document.getElementById("bookInput").style.display = "none";
+  document.getElementById("contactInfo").style.display = "none";
+  document.getElementById("bookList").style.display = "block";
+});
+
+document.getElementById("inputLink").addEventListener("click", () => {
+  document.getElementById("bookList").style.display = "none";
+  document.getElementById("contactInfo").style.display = "none";
+  document.getElementById("bookInput").style.display = "block";
+});
+document.getElementById("contactLink").addEventListener("click", () => {
+  document.getElementById("bookInput").style.display = "none";
+  document.getElementById("bookList").style.display = "none";
+  document.getElementById("contactInfo").style.display = "block";
 });
